5129e83536d2df342ab73c6b44a487cd18ee7195
lxq-yx <62704437+lxq-yx@users.noreply.github.com>
2020-09-06 10:13:40 +0800

    Add files via upload

---
+++ T10.r
@@ -0,0 +1,20 @@
+library(fBasics)
+da=read.table("C:/Users/liaoxiaoqin/Desktop/r-data/data/Tsay3 data/Tsay3 data/d-ibm3dx7008.txt",header=T)
+dim(da)
+da[1,]
+ibm=da[,2]
+sibm=ibm*100
+basicStats(sibm)
+mean(sibm)
+var(sibm)
+sqrt(var(sibm))
+skewness(sibm)
+kurtosis(sibm)
+s1=skewness(sibm)
+t1=s1/sqrt(6/9845)
+t1
+pv=2*(1-pnorm(t1))
+pv
+libm=log(ibm+1)*100
+t.test(libm)
+normalTest(libm,method='jb')
